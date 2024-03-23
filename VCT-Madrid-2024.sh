36a296c228fcf4dad68934e6969543e6340a4e70
zephrous <71692327+TheDaniel3131@users.noreply.github.com>
2024-03-24 00:54:47 +0800

    Update VCT-Madrid-2024.sh

--- VCT-Madrid-2024.sh
+++ VCT-Madrid-2024.sh
@@ -2,8 +2,8 @@
 function watching {
 	echo -e "Watching VCT Madrid 2024 -> SEN Vs PRX Losers Grand Finals!"
 	now=$(date +"%Y-%m-%d %H:%M:%S")
-    echo -e "Date & Time: $now"
+    	echo -e "Date & Time: $now"
 	sleep 0.99
-	watch_match
+	watching
 }
 watching
