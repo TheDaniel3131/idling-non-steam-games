803fb38b437fe66718d7110a6ddf201a4b783f8f
zephrous <danielpoh3131@gmail.com>
2024-03-24 00:54:04 +0800

    new

---
+++ VCT-Madrid-2024-Live.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while true
+do
+	echo -e "Watching VCT Madrid 2024 -> SEN Vs PRX Losers Grand Finals!"
+	now=$(date +"%Y-%m-%d %H:%M:%S")
+    echo -e "Date & Time: $now"
+	sleep 1
+done
---
+++ VCT-Madrid-2024.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+function watching {
+	echo -e "Watching VCT Madrid 2024 -> SEN Vs PRX Losers Grand Finals!"
+	now=$(date +"%Y-%m-%d %H:%M:%S")
+    echo -e "Date & Time: $now"
+	sleep 0.99
+	watch_match
+}
+watching
