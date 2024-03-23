275c898942b232c31c4b2e6e1846a37df1542e48
zephrous <71692327+TheDaniel3131@users.noreply.github.com>
2024-03-24 00:54:23 +0800

    Update VCT-Madrid-2024-Live.sh

--- VCT-Madrid-2024-Live.sh
+++ VCT-Madrid-2024-Live.sh
@@ -3,6 +3,6 @@ while true
 do
 	echo -e "Watching VCT Madrid 2024 -> SEN Vs PRX Losers Grand Finals!"
 	now=$(date +"%Y-%m-%d %H:%M:%S")
-    echo -e "Date & Time: $now"
+    	echo -e "Date & Time: $now"
 	sleep 1
 done
