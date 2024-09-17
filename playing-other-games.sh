23487b1f911ea4590405b5fc9a98d6093798ff15
zephrous <71692327+TheDaniel3131@users.noreply.github.com>
2024-09-17 23:16:35 +0800

    Update

--- playing-other-games.sh
+++ playing-other-games.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function watching {
-	echo -e "Running Other Game Applications..."
+	echo -e "Currently Running Other Game Applications..."
 	now=$(date +"%Y-%m-%d %H:%M:%S")
     	echo -e "Date & Time: $now"
 	sleep 1
